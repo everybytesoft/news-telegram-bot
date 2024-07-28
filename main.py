01f93213c0732d0fc6fb2214f4665aeb4d27715d
KharazyanLevon <166710371+KharazyanLevon@users.noreply.github.com>
2024-07-28 13:02:39 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from NewsBot import NewsBot
-from NewsBotForChannel import NewsBotForChannel
+from NewsBotForChannelru import NewsBotForChannelru
 from threading import Thread
 
 
 def main() -> None:
     bot = NewsBot('6781204411:AAHxbmARcWtjT-dBcdTJqubLpuI1LG3-SfE')
-    bot2 = NewsBotForChannel('6781204411:AAHxbmARcWtjT-dBcdTJqubLpuI1LG3-SfE')
+    bot2 = NewsBotForChannelru('6781204411:AAHxbmARcWtjT-dBcdTJqubLpuI1LG3-SfE')
     Thread(target=bot.run).start()
     Thread(target=bot2.run).start()
 
