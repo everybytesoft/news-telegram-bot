b51a0c4eadebf85312148bd40d70cf6eeaaa4563
KharazyanLevon <166710371+KharazyanLevon@users.noreply.github.com>
2024-10-05 12:24:14 +0400

    Update NewsBot.py

--- NewsBot.py
+++ NewsBot.py
@@ -12,7 +12,7 @@ class NewsBot(telebot.async_telebot.AsyncTeleBot):
             apikey="pub_53530a5051470fbecdfa3067a3f2b8bea6829"
         )
         self.list_of_data = {}
-        self.list_of_categorys = {
+        self.list_of_categories = {
             "Горячие": "top",
             "Бизнес": "business",
             "Развлечения": "entertainment",
@@ -50,52 +50,52 @@ class NewsBot(telebot.async_telebot.AsyncTeleBot):
             "sources": None,
         }
         markup = telebot.async_telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button = telebot.async_telebot.types.KeyboardButton("/set_sorces")
+        button = telebot.async_telebot.types.KeyboardButton("/set_sources")
         button2 = telebot.async_telebot.types.KeyboardButton("/set_category")
-        button3 = telebot.async_telebot.types.KeyboardButton("/set_q")
-        button4 = telebot.async_telebot.types.KeyboardButton("/check_news")
+        button3 = telebot.async_telebot.types.KeyboardButton("/set_keyword")
+        button4 = telebot.async_telebot.types.KeyboardButton("/get_filters")
         button5 = telebot.async_telebot.types.KeyboardButton("/get_news")
         markup.add(button, button2, button3, button4, button5)
         await self.send_message(
             message.chat.id,
-            "Привет! Я бот, который будет отправлять тебе новости по любой теме из главных новостей мира и твоей страны.",
+            f"Привет {message.chat.first_name}! Я бот, который будет отправлять тебе новости по любой теме из главных новостей мира и твоей страны.",
             reply_markup=markup,
         )
         await self.send_message(
             message.chat.id,
-            """Вы можете задать:
-1. Источники: По умолчанию все источники.
-2. Категорию новостей: По умолчанию все категории.
-3. Ключевое слово: любое слово на вашем языке. По умолчанию без ключевого слова.""",
+            """Вы можете установить фильтры новостей по этим критериям:
+1. /set_sources Источники (по умолчанию: Все источники)
+2. /set_category Категория  (по умолчанию: Все категории)
+3. /set_keyword Ключевое слово (по умолчанию: Без ключевого слова)""",
         )
         await self.send_message(
             message.chat.id,
-            "Чтобы узнать настройки параметров, выберите команду CheckNews, чтобы получить новости, выберите команду GetNews",
+            "Чтобы узнать настройки фильтров, выберите команду /get_filters, чтобы получить новости, выберите команду /get_news",
         )
 
     async def check_news(self, message: telebot.types.Message):
         self.list_of_data[message.chat.id]["more_news"] = False
         markup = telebot.async_telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button = telebot.async_telebot.types.KeyboardButton("/set_sorces")
+        button = telebot.async_telebot.types.KeyboardButton("/set_sources")
         button2 = telebot.async_telebot.types.KeyboardButton("/set_category")
-        button3 = telebot.async_telebot.types.KeyboardButton("/set_q")
-        button4 = telebot.async_telebot.types.KeyboardButton("/check_news")
+        button3 = telebot.async_telebot.types.KeyboardButton("/set_keyword")
+        button4 = telebot.async_telebot.types.KeyboardButton("/get_filters")
         button5 = telebot.async_telebot.types.KeyboardButton("/get_news")
         markup.add(button, button2, button3, button4, button5)
         await self.send_message(
             message.chat.id,
             f"""Запрос задан по следующим параметрам:
 1. Источник: {self.list_of_data[message.chat.id]["sources"] if self.list_of_data[message.chat.id]["sources"] is not None else "Все источники"}
-2. Категория: {next((key for key, value in self.list_of_categorys.items() if value == self.list_of_data[message.chat.id]["category"]))}
+2. Категория: {next((key for key, value in self.list_of_categories.items() if value == self.list_of_data[message.chat.id]["category"]))}
 3. Ключевое слово: {self.list_of_data[message.chat.id]["q"] if self.list_of_data[message.chat.id]["q"] is not None else "Без ключевого слова"}""",
         )
 
     async def get_news(self, message: telebot.types.Message):
         markup = telebot.async_telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button = telebot.async_telebot.types.KeyboardButton("/set_sorces")
+        button = telebot.async_telebot.types.KeyboardButton("/set_sources")
         button2 = telebot.async_telebot.types.KeyboardButton("/set_category")
-        button3 = telebot.async_telebot.types.KeyboardButton("/set_q")
-        button4 = telebot.async_telebot.types.KeyboardButton("/check_news")
+        button3 = telebot.async_telebot.types.KeyboardButton("/set_keyword")
+        button4 = telebot.async_telebot.types.KeyboardButton("/get_filters")
         button5 = telebot.async_telebot.types.KeyboardButton("/get_news")
         markup.add(button, button2, button3, button4, button5)
         try:
@@ -108,41 +108,35 @@ class NewsBot(telebot.async_telebot.AsyncTeleBot):
                     image=True,
                 )
             )
+            for i in range(
+                len(self.list_of_data[message.chat.id]["top_headlines"]["results"])
+            ):
+                data = self.list_of_data[message.chat.id]["top_headlines"]["results"][i]
+                title = self.escape_md(data["title"])
+                url = self.escape_md_text_link(data["link"])
+                await self.send_photo(
+                    message.chat.id,
+                    self.list_of_data[message.chat.id]["top_headlines"]["results"][i][
+                        "image_url"
+                    ],
+                    f"[{title}]({url})",
+                    parse_mode="MarkdownV2",
+                    reply_markup=markup,
+                )
+            if len(self.list_of_data[message.chat.id]["top_headlines"]["results"]) == 0:
+                await self.send_message(
+                    message.chat.id,
+                    "По вашему запросу ничего не найдено. Попробуйте другой запрос, или подождите некоторое время, когда новости по вашим параметрам появятся",
+                    reply_markup=markup,
+                )
         except Exception:
             await self.send_message(
                 message.chat.id,
                 f"Произошла ошибка: скорее всего, вы неправильно указали название источника или этот источник не существует в нашей базе данных, попробуйте еще раз",
                 reply_markup=markup,
             )
-        for i in range(
-            len(self.list_of_data[message.chat.id]["top_headlines"]["results"])
-        ):
-            data = self.list_of_data[message.chat.id]["top_headlines"]["results"][i]
-            title = self.escape_md(data["title"])
-            url = self.escape_md_text_link(data["link"])
-            await self.send_photo(
-                message.chat.id,
-                self.list_of_data[message.chat.id]["top_headlines"]["results"][i][
-                    "image_url"
-                ],
-                f"[{title}]({url})",
-                parse_mode="MarkdownV2",
-                reply_markup=markup,
-            )
-        if len(self.list_of_data[message.chat.id]["top_headlines"]["results"]) == 0:
-            await self.send_message(
-                message.chat.id,
-                "По вашему запросу ничего не найдено. Попробуйте другой запрос, или подождите некоторое время, когда новости по вашим параметрам появятся",
-                reply_markup=markup,
-            )
-        else:
-            await self.send_message(
-                message.chat.id,
-                "Если вы хотите получить больше новостей, то попробуйте другой запрос, или подождите некоторое время, когда новости по вашим параметрам появятся",
-                reply_markup=markup,
-            )
 
-    async def set_sorces(self, message: telebot.types.Message):
+    async def set_sources(self, message: telebot.types.Message):
         self.list_of_data[message.chat.id]["flag_for_source"] = True
         markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
         button = telebot.types.KeyboardButton("Все источники")
@@ -153,17 +147,17 @@ class NewsBot(telebot.async_telebot.AsyncTeleBot):
             reply_markup=markup,
         )
 
-    async def set_sorces2(self, message: telebot.types.Message):
+    async def set_sources2(self, message: telebot.types.Message):
         self.list_of_data[message.chat.id]["flag_for_source"] = False
         if message.text == "Все источники":
             self.list_of_data[message.chat.id]["sources"] = None
         elif message.text != "Все источники":
             self.list_of_data[message.chat.id]["sources"] = message.text.lower()  # type: ignore
         markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button = telebot.async_telebot.types.KeyboardButton("/set_sorces")
+        button = telebot.async_telebot.types.KeyboardButton("/set_sources")
         button2 = telebot.async_telebot.types.KeyboardButton("/set_category")
-        button3 = telebot.async_telebot.types.KeyboardButton("/set_q")
-        button4 = telebot.async_telebot.types.KeyboardButton("/check_news")
+        button3 = telebot.async_telebot.types.KeyboardButton("/set_keyword")
+        button4 = telebot.async_telebot.types.KeyboardButton("/get_filters")
         button5 = telebot.async_telebot.types.KeyboardButton("/get_news")
         markup.add(button, button2, button3, button4, button5)
         await self.send_message(
@@ -199,19 +193,19 @@ class NewsBot(telebot.async_telebot.AsyncTeleBot):
             button11,
         )
         await self.send_message(
-            message.chat.id, "Выбирите категорию", reply_markup=markup
+            message.chat.id, "Выберите категорию", reply_markup=markup
         )
 
     async def set_category2(self, message: telebot.types.Message):
-        if message.text in self.list_of_categorys:
-            self.list_of_data[message.chat.id]["category"] = self.list_of_categorys[
+        if message.text in self.list_of_categories:
+            self.list_of_data[message.chat.id]["category"] = self.list_of_categories[
                 message.text
             ]
             markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-            button = telebot.async_telebot.types.KeyboardButton("/set_sorces")
+            button = telebot.async_telebot.types.KeyboardButton("/set_sources")
             button2 = telebot.async_telebot.types.KeyboardButton("/set_category")
-            button3 = telebot.async_telebot.types.KeyboardButton("/set_q")
-            button4 = telebot.async_telebot.types.KeyboardButton("/check_news")
+            button3 = telebot.async_telebot.types.KeyboardButton("/set_keyword")
+            button4 = telebot.async_telebot.types.KeyboardButton("/get_filters")
             button5 = telebot.async_telebot.types.KeyboardButton("/get_news")
             markup.add(button, button2, button3, button4, button5)
             await self.send_message(
@@ -237,10 +231,10 @@ class NewsBot(telebot.async_telebot.AsyncTeleBot):
             self.list_of_data[message.chat.id]["q"] = message.text
 
         markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button = telebot.async_telebot.types.KeyboardButton("/set_sorces")
+        button = telebot.async_telebot.types.KeyboardButton("/set_sources")
         button2 = telebot.async_telebot.types.KeyboardButton("/set_category")
-        button3 = telebot.async_telebot.types.KeyboardButton("/set_q")
-        button4 = telebot.async_telebot.types.KeyboardButton("/check_news")
+        button3 = telebot.async_telebot.types.KeyboardButton("/set_keyword")
+        button4 = telebot.async_telebot.types.KeyboardButton("/get_filters")
         button5 = telebot.async_telebot.types.KeyboardButton("/get_news")
         markup.add(button, button2, button3, button4, button5)
         await self.send_message(
@@ -251,20 +245,20 @@ class NewsBot(telebot.async_telebot.AsyncTeleBot):
 
     async def run(self):
         self.register_message_handler(self.start_command, commands=["start"])
-        self.register_message_handler(self.check_news, commands=["check_news"])
+        self.register_message_handler(self.check_news, commands=["get_filters"])
         self.register_message_handler(self.get_news, commands=["get_news"])
-        self.register_message_handler(self.set_sorces, commands=["set_sorces"])
+        self.register_message_handler(self.set_sources, commands=["set_sources"])
         self.register_message_handler(
-            self.set_sorces2,
+            self.set_sources2,
             func=lambda message: True
             and self.list_of_data[message.chat.id]["flag_for_source"] is True,
         )
         self.register_message_handler(self.set_category, commands=["set_category"])
         self.register_message_handler(
             self.set_category2,
-            func=lambda message: message.text in self.list_of_categorys,
+            func=lambda message: message.text in self.list_of_categories,
         )
-        self.register_message_handler(self.set_q, commands=["set_q"])
+        self.register_message_handler(self.set_q, commands=["set_keyword"])
         self.register_message_handler(
             self.set_q2,
             func=lambda message: True
